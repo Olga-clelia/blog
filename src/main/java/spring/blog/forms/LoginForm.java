81c7e560ba44ab9a9e5a6f593aa4ca811fbb8154
Olga-clelia <131864074+Olga-clelia@users.noreply.github.com>
2023-05-17 01:52:38 +0200

    Add files

---
+++ src/main/java/spring/blog/forms/LoginForm.java
@@ -0,0 +1,21 @@
+package spring.blog.forms;
+
+public class LoginForm {
+
+	private String userName;
+
+	private String password;
+	
+	public String getUserName(){
+		return this.userName;
+	}
+	public void setUserName(String userName){
+		this.userName = userName;
+	}
+	public String getPassword(){
+		return this.password;
+	}
+	public void setPassword(String password){
+		this.password = password;
+	}
+}
